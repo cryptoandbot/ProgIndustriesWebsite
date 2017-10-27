a632c835fbca0d0a0ffc593537de567b53b36fb6
coffeeandscripts <erin.saricilar@gmail.com>
2017-10-27 22:50:51 +1100

    Addition of front page elements

--- js/index.js
+++ js/index.js
@@ -8,11 +8,35 @@ var headerSizer = function() {
 		if($(window).scrollTop() <= 50) {
 			$('#header').css('height', '100px');
 			$('#header').css('background-color', 'rgba(255, 255, 255, 0.25)');
+			$('.dropbtn').css('height', '100px');
+			$('.dropbtn').css('padding-top', '16px');
+			$('#headerLogo').css('font-size', '50px');
+			$('#headerLogo').css('margin-top', '25px');
+			$('#headerButton > i').css('font-size', '50px');
+			$('#headerButton > i').css('margin-top', '25px');
 		} else {
 			$('#header').css('height', '50px');
 			$('#header').css('background-color', 'rgba(0, 0, 0, 0.75)');
+			$('.dropbtn').css('height', '50px');
+			$('.dropbtn').css('padding-top', '10px');
+			$('#headerLogo').css('font-size', '25px');
+			$('#headerLogo').css('margin-top', '12.5px');
+			$('#headerButton > i').css('font-size', '25px');
+			$('#headerButton > i').css('margin-top', '12.5px');
 		}
 	})
 }
 
 $(document).ready(headerSizer);
+
+var hideSplash = function() {
+	$(document).scroll(function() {
+		if($(window).scrollTop() < 150) {
+			$('#splashBox').fadeIn('slow');
+		} else {
+			$('#splashBox').fadeOut('slow');
+		}
+	})
+}
+
+$(document).ready(hideSplash);
